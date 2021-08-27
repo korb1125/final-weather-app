711d3cf3c62b9059bdc9349cf3fdbd8a02c228c9
korb1125 <86573031+korb1125@users.noreply.github.com>
2021-08-26 23:52:44 -0600

    added search engine

--- src/app.js
+++ src/app.js
@@ -24,12 +24,14 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#speed");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
     descriptionElement.innerHTML = response.data.weather[0].main;
+    cityElement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -41,7 +43,17 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
-let apiKey = "41824c8a74846b3921b2180280f99e63";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "41824c8a74846b3921b2180280f99e63";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
